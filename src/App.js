968ce1c02d95e8f139a252d3db2683f813345162
Monjur Hasan <51787931+monjurhasan4404@users.noreply.github.com>
2022-12-18 02:03:30 +0600

    Handle-Event and Rerendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import './App.css';
+import Clock from './components/Clock';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-    </div>
-  );
+    console.log('App component rendered');
+    return (
+        <div>
+            <Clock />
+        </div>
+    );
 }
 
 export default App;
